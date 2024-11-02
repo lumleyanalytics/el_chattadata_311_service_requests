c92d0e25afea72001d39bfa9c920be1626fa8962
apratsunrthd <apratsunrthd@gmail.com>
2024-11-02 19:43:03 -0400

    updated cloud functions screwups

---
+++ terraform/cloud_functions_module/main.tf
@@ -0,0 +1,44 @@
+# cloud_functions_module/main.tf
+
+# Define Cloud Function for fetch_to_gcs
+resource "google_cloudfunctions_function" "fetch_to_gcs" {
+  name        = "${var.function_prefix}-fetch-to-gcs"
+  runtime     = "python310"
+  entry_point = "main"
+  region      = var.region
+
+  source_archive_bucket = var.gcs_bucket_name
+  source_archive_object = "fetch_to_gcs.zip"
+
+  trigger_http = true
+
+  environment_variables = {
+    GCS_BUCKET = var.gcs_bucket_name
+  }
+}
+
+# Define Cloud Function for gcs_to_bigquery
+resource "google_cloudfunctions_function" "gcs_to_bigquery" {
+  name        = "${var.function_prefix}-gcs-to-bigquery"
+  runtime     = "python310"
+  entry_point = "main"
+  region      = var.region
+
+  source_archive_bucket = var.gcs_bucket_name
+  source_archive_object = "gcs_to_bigquery.zip"
+
+  trigger_http = true
+}
+
+# Define Cloud Function for gcs_to_snowflake
+resource "google_cloudfunctions_function" "gcs_to_snowflake" {
+  name        = "${var.function_prefix}-gcs-to-snowflake"
+  runtime     = "python310"
+  entry_point = "main"
+  region      = var.region
+
+  source_archive_bucket = var.gcs_bucket_name
+  source_archive_object = "gcs_to_snowflake.zip"
+
+  trigger_http = true
+}
