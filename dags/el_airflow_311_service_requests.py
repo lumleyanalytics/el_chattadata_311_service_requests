f7166be6854b418e69da3a783adf562bcaacafaf
apratsunrthd <apratsunrthd@gmail.com>
2024-11-03 11:09:07 -0500

    updated project id

--- dags/el_airflow_311_service_requests.py
+++ dags/el_airflow_311_service_requests.py
@@ -25,7 +25,7 @@ with DAG(
     from google.cloud import secretmanager
 
     def get_secret(secret_id):
-        project_id = "your-project-id"  # Replace with your actual project ID or fetch from environment variables
+        project_id = "lumley-analytics"  # Replace with your actual project ID or fetch from environment variables
         client = secretmanager.SecretManagerServiceClient()
         name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
         
